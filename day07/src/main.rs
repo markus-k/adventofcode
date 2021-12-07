7bf7d9e6241ed38f4c1506c49aa0c576e94952ad
Markus Kasten <github@markuskasten.eu>
2021-12-07 11:50:28 +0100

    improvement(day07): clean up solution using a more functional approach
    still not ideal though...

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,34 +1,40 @@
 fn main() {
     let input = include_str!("../input.txt");
-    let (position, fuel) = find_cheapest_position(input);
+    let (position, fuel) = find_cheapest_position(input, simple_cost_fn);
 
     println!("Cheapest position: {}", position);
     println!("Total fuel: {}", fuel);
 }
 
+fn simple_cost_fn(final_pos: usize, current_pos: usize) -> usize {
+    (final_pos as i64 - current_pos as i64).abs() as usize
+}
+
 /// Returns (cheapest_position, total_fuel)
-fn find_cheapest_position(input: &str) -> (usize, usize) {
+fn find_cheapest_position<F>(input: &str, cost_fn: F) -> (usize, usize)
+where
+    F: Fn(usize, usize) -> usize,
+{
     let positions = input
         .split(",")
         .map(|n| n.trim().parse().unwrap())
         .collect::<Vec<usize>>();
-    let mut fuels: Vec<usize> = Vec::new();
 
-    for final_pos in 0..(positions.len()) {
-        let mut fuel = 0;
-        for &pos in positions.iter() {
-            fuel += (final_pos as i64 - pos as i64).abs() as usize;
-        }
+    let fuels = (0..positions.len())
+        .map(|final_position| {
+            positions
+                .iter()
+                .map(|&position| cost_fn(final_position, position))
+                .sum()
+        })
+        .collect::<Vec<usize>>();
 
-        fuels.push(fuel);
-    }
-
-    println!("{:?}", fuels);
-
-    let fuel = *fuels.iter().min().unwrap();
-    let pos = fuels.iter().position(|&x| x == fuel).unwrap();
-
-    (pos, fuel)
+    fuels
+        .iter()
+        .enumerate()
+        .min_by_key(|(_, &val)| val)
+        .map(|(idx, &val)| (idx, val)) // this map is quite ugly, not sure how to avoid it
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -38,7 +44,7 @@ mod tests {
     #[test]
     fn test_example_input() {
         let input = "16,1,2,0,4,2,7,1,2,14";
-        let (position, fuel) = find_cheapest_position(input);
+        let (position, fuel) = find_cheapest_position(input, simple_cost_fn);
 
         assert_eq!(position, 2);
         assert_eq!(fuel, 37);
