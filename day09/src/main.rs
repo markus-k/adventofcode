f4f5b7d7d0560bba6d8ad2134a7650544ae98ac3
Markus Kasten <github@markuskasten.eu>
2021-12-09 17:52:43 +0100

    improvement(day09): some cleanup before starting part2

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,9 +1,21 @@
 fn main() {
     let input = include_str!("../input.txt");
     let map = parse_input(input);
+    let lowspots = find_lowspots(&map);
 
-    let risk_level = find_lowspots(&map);
+    let risk_level = risk_level(&map, &lowspots);
     println!("RISK LEVEL: {}", risk_level);
+}
+
+struct Point {
+    x: usize,
+    y: usize,
+}
+
+impl Point {
+    fn new(x: usize, y: usize) -> Self {
+        Self { x, y }
+    }
 }
 
 fn parse_input(input: &str) -> Vec<Vec<u32>> {
@@ -22,29 +34,37 @@ fn map_size(map: &Vec<Vec<u32>>) -> (usize, usize) {
     (map[0].len(), map.len())
 }
 
-fn find_lowspots(map: &Vec<Vec<u32>>) -> u32 {
+fn find_lowspots(map: &Vec<Vec<u32>>) -> Vec<Point> {
     let (width, height) = map_size(map);
-    let mut risk_level = 0;
 
-    for y in 0..height {
-        for x in 0..width {
-            let val = map[y][x];
+    (0..height)
+        .flat_map(|y| {
+            (0..width).filter_map(move |x| {
+                let val = map[y][x];
 
-            let is_lowspot = [(-1isize, 0isize), (1, 0), (0, 1), (0, -1)]
-                .map(|(dy, dx)| ((y as isize + dy), (x as isize + dx)))
-                .iter()
-                .filter(|(ny, nx)| {
-                    *nx >= 0 && *nx < width as isize && *ny >= 0 && *ny < height as isize
-                })
-                .all(|(ny, nx)| val < map[*ny as usize][*nx as usize]);
+                let is_lowspot = [(-1isize, 0isize), (1, 0), (0, 1), (0, -1)]
+                    .map(|(dy, dx)| ((y as isize + dy), (x as isize + dx)))
+                    .iter()
+                    .filter(|(ny, nx)| {
+                        *nx >= 0 && *nx < width as isize && *ny >= 0 && *ny < height as isize
+                    })
+                    .all(|(ny, nx)| val < map[*ny as usize][*nx as usize]);
 
-            if is_lowspot {
-                risk_level += val + 1;
-            }
-        }
-    }
+                if is_lowspot {
+                    Some(Point::new(x, y))
+                } else {
+                    None
+                }
+            })
+        })
+        .collect()
+}
 
-    risk_level
+//fn find_basins(map: &Vec<Vec<u32>>) -> Vec<Vec<(usize, usize)>> {
+//}
+
+fn risk_level(map: &Vec<Vec<u32>>, lowspots: &Vec<Point>) -> u32 {
+    lowspots.iter().map(|point| map[point.y][point.x] + 1).sum()
 }
 
 #[cfg(test)]
@@ -59,7 +79,8 @@ mod tests {
 8767896789
 9899965678";
         let map = parse_input(input);
-        let risk_level = find_lowspots(&map);
+        let lowspots = find_lowspots(&map);
+        let risk_level = risk_level(&map, &lowspots);
 
         assert_eq!(risk_level, 15);
     }
