276a78f6c487dada1640ae825e9acb016a7c0f12
Markus Kasten <github@markuskasten.eu>
2022-12-04 21:41:59 +0100

    feat: add benchmark to 2022 day04

---
+++ 2022/day04/benches/bench.rs
@@ -0,0 +1,15 @@
+use day04::{find_fully_contained, find_overlapping};
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let input = include_str!("../input.txt");
+
+    c.bench_function("part1", |b| {
+        b.iter(|| find_fully_contained(black_box(input)))
+    });
+    c.bench_function("part1", |b| b.iter(|| find_overlapping(black_box(input))));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
--- 2022/day04/src/main.rs
+++ 2022/day04/src/main.rs
@@ -1,4 +1,4 @@
-use std::ops::RangeInclusive;
+use day04::{find_fully_contained, find_overlapping};
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -11,74 +11,4 @@ fn main() {
         "At all overlapping section ids: {}",
         find_overlapping(input)
     );
-}
-
-fn parse_sections(sections: &str) -> Option<RangeInclusive<u32>> {
-    let (start, end) = sections.split_once('-')?;
-
-    Some((start.parse().unwrap())..=(end.parse().unwrap()))
-}
-
-trait ContainsRange {
-    fn fully_contains_range(&self, other: &Self) -> bool;
-    fn overlaps_range(&self, other: &Self) -> bool;
-}
-
-impl<U> ContainsRange for RangeInclusive<U>
-where
-    U: PartialOrd<U>,
-{
-    fn fully_contains_range(&self, other: &Self) -> bool {
-        other.start() >= self.start() && other.end() <= self.end()
-    }
-
-    fn overlaps_range(&self, other: &Self) -> bool {
-        (other.start() >= self.start() && other.start() <= self.end())
-            || (other.end() <= self.end() && other.end() >= self.start())
-    }
-}
-
-fn parse_input(
-    input: &str,
-) -> impl Iterator<Item = (RangeInclusive<u32>, RangeInclusive<u32>)> + '_ {
-    input
-        .lines()
-        .filter_map(|line| line.split_once(','))
-        .filter_map(|(first, second)| Some((parse_sections(first)?, parse_sections(second)?)))
-}
-
-fn find_fully_contained(input: &str) -> usize {
-    parse_input(input)
-        .filter(|(first, second)| {
-            first.fully_contains_range(second) || second.fully_contains_range(first)
-        })
-        .count()
-}
-
-fn find_overlapping(input: &str) -> usize {
-    parse_input(input)
-        .filter(|(first, second)| first.overlaps_range(second) || second.overlaps_range(first))
-        .count()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    const EXAMPLE_INPUT: &str = "2-4,6-8
-2-3,4-5
-5-7,7-9
-2-8,3-7
-6-6,4-6
-2-6,4-8";
-
-    #[test]
-    fn test_example_input_part1() {
-        assert_eq!(find_fully_contained(EXAMPLE_INPUT), 2);
-    }
-
-    #[test]
-    fn test_example_input_part2() {
-        assert_eq!(find_overlapping(EXAMPLE_INPUT), 4);
-    }
 }
