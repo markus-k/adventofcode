d723382ca52f0d74431b5202e7cd9ebd29701e4f
Markus Kasten <github@markuskasten.eu>
2022-12-18 13:10:14 +0100

    feat(2022/day18): add part2

--- 2022/day18/src/lib.rs
+++ 2022/day18/src/lib.rs
@@ -1,20 +1,35 @@
-#[derive(Debug, PartialEq, Eq)]
-pub struct Point3D(usize, usize, usize);
+use std::collections::HashSet;
 
-macro_rules! eq_with_offset {
-    ($a:ident, $b:ident, $x:literal, $y:literal, $z:literal) => {
-        ($a.0 == $b.0 + $x && $a.1 == $b.1 + $y && $a.2 == $b.2 + $z)
-    };
-}
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct Point3D(isize, isize, isize);
 
 impl Point3D {
     pub fn is_adjacent(&self, other: &Point3D) -> bool {
+        macro_rules! eq_with_offset {
+            ($a:ident, $b:ident, $x:literal, $y:literal, $z:literal) => {
+                ($a.0 == $b.0 + $x && $a.1 == $b.1 + $y && $a.2 == $b.2 + $z)
+            };
+        }
+
         eq_with_offset!(self, other, 1, 0, 0)
             || eq_with_offset!(other, self, 1, 0, 0)
             || eq_with_offset!(self, other, 0, 1, 0)
             || eq_with_offset!(other, self, 0, 1, 0)
             || eq_with_offset!(self, other, 0, 0, 1)
             || eq_with_offset!(other, self, 0, 0, 1)
+    }
+
+    pub fn adjacent(&self) -> [Point3D; 6] {
+        let offsets = [
+            (1, 0, 0),
+            (-1, 0, 0),
+            (0, 1, 0),
+            (0, -1, 0),
+            (0, 0, 1),
+            (0, 0, -1),
+        ];
+
+        offsets.map(|offset| Point3D(self.0 + offset.0, self.1 + offset.1, self.2 + offset.2))
     }
 }
 
@@ -43,6 +58,69 @@ pub fn part1(cubes: &[Point3D]) -> usize {
     count
 }
 
+pub fn part2(cubes: &[Point3D]) -> usize {
+    let mut grid = HashSet::<Point3D>::new();
+    grid.extend(cubes.iter().cloned());
+
+    let lava_bounds = grid.iter().fold(
+        (
+            (isize::MAX, isize::MIN),
+            (isize::MAX, isize::MIN),
+            (isize::MAX, isize::MIN),
+        ),
+        |acc, cube| {
+            (
+                (acc.0 .0.min(cube.0), acc.0 .1.max(cube.0)),
+                (acc.1 .0.min(cube.1), acc.1 .1.max(cube.1)),
+                (acc.2 .0.min(cube.2), acc.2 .1.max(cube.2)),
+            )
+        },
+    );
+
+    // encapsulate our lava in water, one block bigger in each direction
+    // than the lava bounds
+    let start = Point3D(
+        lava_bounds.0 .0 - 1,
+        lava_bounds.1 .0 - 1,
+        lava_bounds.2 .0 - 1,
+    );
+
+    // depth first search
+    let mut s = Vec::with_capacity(6);
+    s.push(start);
+    let mut water_blocks = HashSet::new();
+
+    while let Some(v) = s.pop() {
+        if !water_blocks.contains(&v) {
+            for w in v.adjacent() {
+                if w.0 >= lava_bounds.0 .0 - 1
+                    && w.0 <= lava_bounds.0 .1 + 1
+                    && w.1 >= lava_bounds.1 .0 - 1
+                    && w.1 <= lava_bounds.1 .1 + 1
+                    && w.2 >= lava_bounds.2 .0 - 1
+                    && w.2 <= lava_bounds.2 .1 + 1
+                    && !grid.contains(&w)
+                {
+                    s.push(w);
+                }
+            }
+
+            water_blocks.insert(v);
+        }
+    }
+
+    // then see how many water block touch lava
+    water_blocks
+        .iter()
+        .map(|cube| {
+            cube.adjacent()
+                .iter()
+                .filter(|adj| grid.contains(adj))
+                .count()
+        })
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -66,5 +144,12 @@ mod tests {
         let cubes = parse_input(EXAMPLE_INPUT);
 
         assert_eq!(part1(&cubes), 64);
+    }
+
+    #[test]
+    fn test_example_input_part2() {
+        let cubes = parse_input(EXAMPLE_INPUT);
+
+        assert_eq!(part2(&cubes), 58);
     }
 }
--- 2022/day18/src/main.rs
+++ 2022/day18/src/main.rs
@@ -1,8 +1,11 @@
-use day18::{parse_input, part1};
+use day18::{parse_input, part1, part2};
 
 fn main() {
     let input = include_str!("../input.txt");
     let cubes = parse_input(input);
     let part1 = part1(&cubes);
     println!("part1: {part1}");
+
+    let part2 = part2(&cubes);
+    println!("part2: {part2}");
 }
